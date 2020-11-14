6e61bbfd4f49121c8f64722e92154d578e5f8a5e
Peimou <ps3136@columbia.edu>
2020-11-14 00:57:41 -0500

    update Data 1

--- InsertData.sql
+++ InsertData.sql
@@ -1,41 +1,16 @@
-COPY State_info(state_name, gdp, governor)
-FROM 'State_info.csv'
-DELIMITER ','
-CSV HEADER;
+\copy State_info FROM 'State_info.csv' with (format csv,header true, delimiter ',')
 
-COPY City(population, avg_salary, new_cases, death, city_name, state_name)
-FROM 'City_info.csv'
-DELIMITER ','
-CSV HEADER;
+\copy City FROM 'City_info.csv' with (format csv,header true, delimiter ',')
 
-COPY Hospital(facility_id, facility_name, zipcode, ownership, emergency_service, overall_rating, city_name, state_name)
-FROM 'Hospital.csv'
-DELIMITER ','
-CSV HEADER;
+\copy Hospital FROM 'Hospital.csv' with (format csv,header true, delimiter ',')
 
-COPY Patient_hospitalization(admission_date, discharge_date, age, race, facility_id, id)
-FROM 'Patient_hospitalization.csv'
-DELIMITER ','
-CSV HEADER
-;
+\copy Patient_hospitalization FROM 'Patient_hospitalization.csv' with (format csv,header true, delimiter ',')
 
-COPY Policy_published_by_state(policy_type, details, state_name, created_time)
-FROM 'Policy_published_by_state.csv'
-DELIMITER ','
-CSV HEADER
-;
+\copy User_info FROM 'User_info.csv' with (format csv,header true, delimiter ',')
 
-COPY User_info(username, age, gender, race)
-FROM 'User_info.csv'
-DELIMITER ','
-CSV HEADER
-;
+\copy Comment FROM 'Comment.csv' with (format csv,header true, delimiter ',')
 
-COPY Comment(id, attitude, details, comment_created_time)
-FROM 'Comment.csv'
-DELIMITER ','
-CSV HEADER
-;
+\copy Policy_published_by_state FROM 'Policy_published_by_state.csv' with (format csv,header true, delimiter ',')
 
 
 
