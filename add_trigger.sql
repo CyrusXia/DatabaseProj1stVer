3c9861af213ca67e225d9febfeeda3c4097b649b
Peimou <ps3136@columbia.edu>
2020-12-06 15:32:03 -0500

    Update trigger

--- add_trigger.sql
+++ add_trigger.sql
@@ -1,10 +1,38 @@
-CREATE TRIGGER CityTotalCase
-AFTER UPDATE OF new_cases ON City
-REFERENCING OLD ROW AS OldTuple
-	    NEW ROW AS NewTuple
-FOR EACH ROW
-WHEN (NewTuple.new_cases != 0)
-    UPDATE City
-    SET total_cases = OldTuple.total_cases + NewTuple.new_cases
-    WHERE state_name = NewTuple.state_name 
-        AND city_name = NewTuple.city_name
+DROP TABLE IF EXISTS City_evaluation;
+
+CREATE TYPE evaluation AS ENUM ('Low risk', 'High risk');
+
+CREATE TABLE City_evaluation(
+state_name VARCHAR(40),
+city_name VARCHAR(40),
+evaluate_time TIMESTAMP,
+evaluation evaluation,
+PRIMARY KEY (state_name, city_name, evaluate_time),
+FOREIGN KEY (state_name, city_name) REFERENCES City
+	ON DELETE CASCADE
+);
+
+CREATE OR REPLACE FUNCTION update_city_evaluation_func()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL AS
+$$
+BEGIN
+	IF NEW.new_cases / NEW.population > 0.1 THEN
+		INSERT INTO City_evaluation(state_name, city_name, evaluate_time, evaluation)
+		VALUES(NEW.state_name, NEW.city_name, now(), 'High risk');
+	ELSE
+		INSERT INTO City_evaluation(state_name, city_name, evaluate_time, evaluation)
+		VALUES(NEW.state_name, NEW.city_name, now(), 'Low risk');
+	END IF;
+
+	RETURN NEW;
+
+END;
+$$;
+
+CREATE TRIGGER update_city_evaluation
+  BEFORE UPDATE
+  ON City
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_city_evaluation_func();
+
