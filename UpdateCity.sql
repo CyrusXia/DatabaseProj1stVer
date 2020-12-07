1f8bbd6487b25d79e890aee939ead1f62fe121ed
Peimou <ps3136@columbia.edu>
2020-12-07 11:57:10 -0500

    update city data test

---
+++ UpdateCity.sql
@@ -0,0 +1,11 @@
+UPDATE City SET new_cases=1000 WHERE city_name='Abbeville' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='Adamsville' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='Alabaster' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='Albertville' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='AlexanderCity' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='Aliceville' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='Athens' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='BayMinette' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='Brighton' AND state_name='Alabama';
+UPDATE City SET new_cases=1000 WHERE city_name='CenterPoint' AND state_name='Alabama';
+
